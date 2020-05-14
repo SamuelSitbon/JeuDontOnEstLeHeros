f78b520a1973d939555902ee7e7d4d4927df54a7
Samuel Sitbon <samuel.sitbon.paroissien@gmail.com>
2020-05-14 12:29:55 +0200

    Fin de la Section 4

---
+++ Jeudontonestlehéros.WEB.UI/Controllers/AventureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jeudontonestlehéros.WEB.UI.Controllers
+{
+    public class AventureController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.ViewBag.MonTitre = "Aventures";
+            return View();
+        }
+    }
+}
