c656ee26e72948e9115cce2caa366ab45aa7e60e
Samuel Sitbon <samuel.sitbon.paroissien@gmail.com>
2020-05-27 12:13:47 +0200

    Fin réel de la Quête 8

--- jeudontonestleheros.BackOffice.Web.UI/Controllers/ParagrapheController.cs
+++ jeudontonestleheros.BackOffice.Web.UI/Controllers/ParagrapheController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,16 +10,34 @@ namespace jeudontonestleheros.BackOffice.Web.UI.Controllers
 {
     public class ParagrapheController : Controller
     {
+        #region A Supprimer après Entities
+        private List<Paragraphe> _maList = new List<Paragraphe>()
+        {
+            new Paragraphe() {Id = 1, Numero = 1, Titre = "Titre 1" },
+            new Paragraphe() {Id = 2, Numero = 10, Titre = "Titre 2" },
+            new Paragraphe() {Id = 5, Numero = 14, Titre = "Titre 3" }
+        };
+        #endregion
+
         #region Méthodes publiques
         public ActionResult Create()
         {
-            return this.View();
+            return View();
         }
 
         [HttpPost]
         public ActionResult Create(Paragraphe paragraphe)
         {
-            return this.View();
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Paragraphe paragraphe = null;
+
+            paragraphe = _maList.First(item => item.Id == id);
+
+            return View(paragraphe);
         }
         #endregion
     }
